65b3a8a220b7b97e6bbf5a130911cb33b121b552
Bianca <bianca.raiane18@gmail.com>
2026-03-27 18:54:53 -0300

    refactor: :recycle: set up app contexts, public routes, and theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,28 @@
-import { useState } from "react";
+import { ThemeToggle } from "./components/ThemeToggle/ThemeToggle";
 
-import reactLogo from "./assets/react.svg";
+import { Router } from "wouter";
 
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Login } from "./app/(public)/Login/Login";
+import { AuthProvider } from "./context/provider";
+import { ThemeProvider } from "./context/ThemeProvider";
+import { PublicRoute } from "./guards/PublicRoute";
+import QueryProvider from "./QueryProvider";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test feature/teste
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <QueryProvider>
+      <ThemeProvider>
+        <Router>
+          <AuthProvider>
+            <div className="fixed pt-4 pl-4 z-50 right-0">
+              <ThemeToggle />
+            </div>
+            <PublicRoute path="/auth" component={Login} />
+            <PublicRoute path="/" component={Login} />
+          </AuthProvider>
+        </Router>
+      </ThemeProvider>
+    </QueryProvider>
   );
 }
 

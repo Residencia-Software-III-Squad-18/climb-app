f3133b644e17613e93defcb96bcdaa7d6e31875b
Bianca <bianca.raiane18@gmail.com>
2026-03-27 19:05:02 -0300

    refactor: :recycle: update configs and modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["DM Sans", "sans-serif"],
+      },
+      colors: {
+        primary: "#2BBFA4",
+        "dark-bg": "#0E1822",
+        "light-bg": "#ffffff",
+        "dark-text": "#ffffff",
+        "light-text": "#0F1B2D",
+      },
+    },
   },
+  darkMode: "class",
   plugins: [],
 };

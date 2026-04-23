3ba015b58d9815d29095d7ea8b38f3d7f14a1b4f
Bianca <bianca.raiane18@gmail.com>
2026-04-23 01:01:25 -0300

    feat: :sparkles: create auth context and zustand stores to persist user session data across pages and inserting config for private and public routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryProvider } from "@/context/QueryProvider";
+import { AuthProvider } from "@/context";
+import { PrivateRoute } from "@/guards/PrivateRoute";
+import { PublicRoute } from "@/guards/PublicRoute";
 import Index from "./pages/Index.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
 import RecuperarSenha from "./pages/RecuperarSenha.tsx";
@@ -14,30 +17,97 @@ import Documentos from "./pages/Documentos.tsx";
 import Contratos from "./pages/Contratos.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/recuperar-senha" element={<RecuperarSenha />} />
-          <Route path="/solicitar-acesso" element={<SolicitarAcesso />} />
-          <Route path="/agenda" element={<Agenda />} />
-          <Route path="/permissoes" element={<Permissoes />} />
-          <Route path="/empresas" element={<Empresas />} />
-          <Route path="/documentos" element={<Documentos />} />
-          <Route path="/contratos" element={<Contratos />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <QueryProvider>
+    <BrowserRouter>
+      <TooltipProvider>
+        <AuthProvider>
+          <Toaster />
+          <Sonner />
+          <Routes>
+            {/* Rotas Públicas */}
+            <Route
+              path="/"
+              element={
+                <PublicRoute>
+                  <Index />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/recuperar-senha"
+              element={
+                <PublicRoute>
+                  <RecuperarSenha />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/solicitar-acesso"
+              element={
+                <PublicRoute>
+                  <SolicitarAcesso />
+                </PublicRoute>
+              }
+            />
+
+            {/* Rotas Privadas */}
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/agenda"
+              element={
+                <PrivateRoute>
+                  <Agenda />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/permissoes"
+              element={
+                <PrivateRoute>
+                  <Permissoes />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/empresas"
+              element={
+                <PrivateRoute>
+                  <Empresas />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/documentos"
+              element={
+                <PrivateRoute>
+                  <Documentos />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/contratos"
+              element={
+                <PrivateRoute>
+                  <Contratos />
+                </PrivateRoute>
+              }
+            />
+
+            {/* Rota 404 */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AuthProvider>
+      </TooltipProvider>
+    </BrowserRouter>
+  </QueryProvider>
 );
 
 export default App;
